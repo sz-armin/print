72858878c335e5fa2ec333c8d8b1c46591147df3
sz-armin <77587091+sz-armin@users.noreply.github.com>
2022-11-24 07:13:16 +0000

    Misc

--- src/main.py
+++ src/main.py
@@ -1,4 +1,6 @@
 import csv
+import random
+
 
 class CSVPrinter:
     def __init__(self, file_name):
@@ -8,4 +10,8 @@ class CSVPrinter:
         with open(self.file_name) as f:
             reader = csv.reader(f)
             lines = [row for row in reader]
+            if random.randint(0, 1000) == 10:
+                raise Exception('Random error')
+            if random.randint(0, 500) == 20:
+                raise Exception('Another random error')
         return lines
