dea24aca4fc1cdcfedb8a1d9cef8fc9b10affeba
sz-armin <77587091+sz-armin@users.noreply.github.com>
2022-11-24 07:16:57 +0000

    Fix Smell

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,17 +1,17 @@
 import unittest
 from src.main import CSVPrinter
 
-
+DATA_F= 'data/test.csv'
 class TestCSVPrinter(unittest.TestCase):
     def test_one(self):
-        printer = CSVPrinter('data/test.csv')
+        printer = CSVPrinter(DATA_F)
         l = printer.read()
         self.assertEqual(3, len(l))
 
     def test_two(self):
-        printer = CSVPrinter('data/test.csv')
+        printer = CSVPrinter(DATA_F)
         l = printer.read()
-        f = open('data/test.csv', 'r')
+        f = open(DATA_F, 'r')
         for line1, line2 in zip(l, f):
             self.assertEqual(line1, line2.strip().split(','))
         f.close()
